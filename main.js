e5f5183825162a846d559cd5d0bdcfff9b2eb723
AbuAbdurrohman <kambaralyabytaliev6@gmail.com>
2024-02-16 14:55:18 +0600

    initial

---
+++ main.js
@@ -0,0 +1,65 @@
+const obj = {
+    name: 'Kambaraly',
+    surname: 'Abytaliev',
+    phone: '+996 505 01 01 01',
+    married: 'no',
+    gender: 'male',
+    address: {
+        country: 'Kyrgyzstan',
+        city: 'Osh',
+        street: 'Kalmatova 55A'
+    },
+    friends: [
+        {
+        name: 'Orozbiy',
+        surname: 'Gadaev',
+        phone: '+996 777 88 88 88',
+        married: 'no',
+        gender: 'male',
+        address: {
+            country: 'Kyrgyzstan',
+            region: 'Osh',
+            city: 'Aravan',
+            street: 'I dont know'
+        },
+    },
+    {
+        name: 'Guljigit',
+        surname: 'Janibekov',
+        phone: '+996 555 18 17 16',
+        married: 'no',
+        gender: 'male',
+        address: {
+            country: 'Kyrgyzstan',
+            region: 'Osh',
+            city: 'Osh',
+            street: 'Raimbekova'
+        },
+    },
+    {
+        name: 'Erjigit',
+        surname: 'I dont know',
+        phone: '+996 223 77 66 55',
+        married: 'no',
+        gender: 'male',
+        address: {
+            country: 'Kyrgyzstan',
+            region: 'Osh',
+            city: 'Osh',
+            street: 'Ak Tilek'
+        },
+    },
+    {
+        name: 'Muhammed',
+        surname: 'Ahunov',
+        phone: '+996 506 02 02 02',
+        married: 'no',
+        gender: 'male',
+        address: {
+            country: 'Kyrgyzstan',
+            region: 'Osh',
+            city: 'Osh'
+        },
+    },
+    ]
+}
